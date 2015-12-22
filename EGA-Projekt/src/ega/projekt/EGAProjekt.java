eb177b747cacdaac2e1bb6a2e3a520497dcb6c8a
candiehl <diehlcan@gmail.com>
2015-12-22 11:44:06 +0100

    Created Project
    Created Project and added the GraphWindow Class and a main class.

---
+++ EGA-Projekt/src/ega/projekt/EGAProjekt.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ega.projekt;
+
+import ega.projekt.gui.GraphWindow;
+
+/**
+ *
+ * @author diehl
+ */
+public class EGAProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GraphWindow gwin = new GraphWindow();
+        gwin.setVisible(true);
+    }
+    
+}
